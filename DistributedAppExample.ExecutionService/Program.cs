08654d2fa8f709b68d561aca623be2489020968a
Pradip Khakurel <pradip.khakurel@gmail.com>
2016-10-27 23:00:31 +0200

    modify namespace

--- DistributedAppExample.ExecutionService/Program.cs
+++ DistributedAppExample.ExecutionService/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistributedAppExample.ExecutionService
+namespace DistributedAppExample.ReaderService
 {
     class Program
     {
--- DistributedAppExample.LauncherService/Properties/AssemblyInfo.cs
+++ DistributedAppExample.LauncherService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DistributedAppExample.LauncherService")]
+[assembly: AssemblyTitle("DistributedAppExample.DistributorService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DistributedAppExample.LauncherService")]
+[assembly: AssemblyProduct("DistributedAppExample.DistributorService")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
