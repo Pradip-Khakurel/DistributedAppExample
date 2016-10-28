182838452df3ce8adc148eef9a7de4f1f8637fa8
Pradip Khakurel <pradip.khakurel@gmail.com>
2016-10-28 22:10:53 +0200

    edit

---
+++ DistributedAppExample.Shared.IO/ReaderMasterActor.cs
@@ -0,0 +1,14 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedAppExample.Shared.IO
+{
+    public class ReaderMasterActor : ReceiveActor
+    {
+
+    }
+}
---
+++ DistributedAppExample.Shared/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedAppExample.Shared
+{
+    public class Message
+    {
+        public string Content { get; }
+
+        public Message(string content)
+        {
+            Content = content;
+        }
+    }
+}
